7268f03044e0e294fcdee1c7297f68603e2c6fca
Chien-Chi Lo <chien-chi@pn1704856.lanl.gov>
2019-01-16 15:55:20 -0700

    add gottcha2 db download to referenece directory and DockerFile

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,33 @@ MAINTAINER KBase Developer
 # install line here, a git checkout to download code, or run any other
 # installation scripts.
 
-# RUN apt-get update
+# Update
+#RUN apt-get update
+
+# Here we install a python coverage tool and an
+# https library that is out of date in the base image.
+RUN pip install coverage
+
+# For minimap2 bin
+WORKDIR /kb/module
+RUN \
+    git clone https://github.com/lh3/minimap2 && \
+    cd minimap2 && make && \
+    export PATH=/kb/module/minimap2:$PATH
+
+# For Krona Tools
+RUN \
+    git clone https://github.com/marbl/Krona && \
+    cd Krona/KronaTools && \
+    ./install.pl && \
+    export PATH=/usr/local/bin:$PATH
+#    ./install.pl && \
+#    mkdir taxonomy && \
+#    ./updateTaxonomy.sh && \
+#    ./updateAccessions.sh
+
+# For gottcha2 dbs (rest of db installation to ref data mount in entrypoint.sh init script)
+RUN mkdir -p /data/gottcha2
 
 
 # -----------------------------------------
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,6 +17,28 @@ elif [ "${1}" = "async" ] ; then
   sh ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
+  mkdir -p /data/gottcha2
+  cd /data/gottcha2
+
+  echo "downloading: https://edge-dl.lanl.gov/GOTTCHA2/RefSeq-Release90/taxdump.tar.gz"
+  mkdir -p /data/gottcha2/RefSeq90
+  cd /data/gottcha2/RefSeq90
+  wget -q https://edge-dl.lanl.gov/GOTTCHA2/RefSeq-Release90/taxdump.tar.gz
+
+  echo "downloading: https://edge-dl.lanl.gov/GOTTCHA2/RefSeq-Release90/RefSeq-r90.cg.BacteriaViruses.species.fna.tar"
+  wget -q https://edge-dl.lanl.gov/GOTTCHA2/RefSeq-Release90/RefSeq-r90.cg.BacteriaViruses.species.fna.tar
+  tar -xf RefSeq-r90.cg.BacteriaViruses.species.fna.tar
+  rm RefSeq-r90.cg.BacteriaViruses.species.fna.tar
+
+   cd /data/gottcha2
+#  if [ -s "/data/gottcha2/RefSeq90/RefSeq-r90.cg.BacteriaViruses.species.fna" ] ; then
+  if [ -s "/data/gottcha2/RefSeq90/RefSeq-r90.cg.BacteriaViruses.species.fna" -a -s "/data/gottcha2/RefSeq90/taxdump.tar.gz" ] ; then
+    echo "DATA DOWNLOADED SUCCESSFULLY"
+    touch /data/__READY__
+  else
+    echo "Init failed"
+  fi  
+
 elif [ "${1}" = "bash" ] ; then
   bash
 elif [ "${1}" = "report" ] ; then
